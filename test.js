0b03350dedcefa1282a7fd8860b2f4711256457a
byshlata <byshlata@yandex.ru>
2021-11-15 23:05:43 +0300

    WEST-EAST

--- test.js
+++ test.js
@@ -1,11 +1,43 @@
-function findUniq(arr) {
-    arr.sort();
-    if (arr[0] == arr[1]){
-        return arr[arr.length-1];
+function dirReduc(arr){
+    let arrayNamber = [];
+
+    for ( let i = 0; i < arr.length; i++){
+        if (arr[i] == 'NORTH' ){
+            arrayNamber[i] = 1;
+        } 
+        if (arr[i] == 'SOUTH' ){
+            arrayNamber[i] = -1;
+        }
+        if (arr[i] == 'EAST'){
+            arrayNamber[i] = 2;
+        }
+        if (arr[i] == 'WEST'){
+            arrayNamber[i] = -2;
+        }
     }
-    else {
-        return arr[0];
+    for ( let i = 0; i < arr.length - 1; i++){
+        if ((arrayNamber[i] + arrayNamber[i+1]) == 0){
+            arrayNamber.splice(i, 2);
+            i = -1;
+        }
     }
+    let arrayNamberOff = [];
+    for ( let i = 0; i < arrayNamber.length; i++){
+        if (arrayNamber[i] ==  1){
+            arrayNamberOff[i] = 'NORTH';
+        } 
+        if (arrayNamber[i] == -1){
+            arrayNamberOff[i] = 'SOUTH';
+        }
+        if (arrayNamber[i] == 2){
+            arrayNamberOff[i] = 'EAST';
+        }
+        if (arrayNamber[i] == -2){
+            arrayNamberOff[i] = 'WEST';
+        }
+    }
+
+    return arrayNamberOff;
   }
-    console.log (findUniq([ 0, 1, 0 ]));
+    console.log (dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]));
 
