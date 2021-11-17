5edab8398b36563db5ca9ebcfe9e65206ba833c2
byshlata <byshlata@yandex.ru>
2021-11-17 20:36:44 +0300

    Игра судоку

--- test.js
+++ test.js
@@ -1,31 +1,60 @@
-function score( dice ) {
-    dice.sort();
-    let points = 0;
-    for (let i = 0; i < 3; i++){
-
-        if ((dice[i] == dice[i+1]) && (dice[i]== dice[i+2]) ){
-            if (dice[i] != 1){
-                points = dice[i] * 100;
-            } else {
-                points = 1000;
-            }
-            dice.splice(i, 1);
-            dice.splice(i, 1);
-            dice.splice(i, 1);
-            i = Infinity;
-        }
+function doneOrNot(board){
+  let sumRows = 0;
+  let sumColumns = 0;
+  for (let i = 0; i < 9; i++){
+    for (let k = 0; k < 9; k++){
+      sumRows += board[i][k];
     }
- for ( let i = 0; i < dice.length; i++){
-     if ( dice [i] == 1) {
-         points = points + 100;
-     }
-     if ( dice [i] == 5) {
-         points = points + 50;
-     }
- }
-        return points;
+    if (sumRows != 45){
+      return 'Try again!';
     }
+    sumRows = 0;
+  }
+
+  for (let i = 0; i < 9; i++){
+    for (let k = 0; k < 9; k++){
+      sumColumns += board[k][i];
+    }
+    if (sumColumns != 45){
+      return 'Try again!';
+    }
+    sumColumns = 0;
+  }
 
 
-    console.log (score([2, 4, 4, 5, 4]));
+  if ((sumRegion(0, 0, board) == false) || (sumRegion(3, 0, board) == false) || (sumRegion(6, 0, board) == false) || (sumRegion(0, 3, board) == false) || (sumRegion(3, 3, board) == false) || (sumRegion(6, 3, board) == false) || (sumRegion(0, 6, board) == false) || (sumRegion(3, 6, board) == false) || (sumRegion(6, 6, board) == false)){
+    return 'Try again!';
+  }
+
+
+ return 'Finished!';
+}
+
+
+function sumRegion(i, k, array){
+  let sum = 0;
+  let row = i + 3;
+  let col = k + 3;
+  for (i; i < row; i++){
+    for (k; k < col; k++){
+      sum += array [i][k];
+    }
+    k = k -3;
+  }
+  if (sum != 45){
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+    console.log (doneOrNot([[ 5, 3, 4, 6, 7, 8, 9, 1, 2 ],
+                            [ 6, 7, 2, 1, 9, 5, 3, 4, 8 ],
+                            [ 1, 9, 8, 3, 4, 2, 5, 6, 7 ],
+                            [ 8, 5, 9, 7, 6, 1, 4, 2, 3 ],
+                            [ 4, 2, 6, 8, 5, 3, 7, 9, 1 ],
+                            [ 7, 1, 3, 9, 2, 4, 8, 5, 6 ],
+                            [ 9, 6, 1, 5, 3, 7, 2, 8, 4 ],
+                            [ 2, 8, 7, 4, 1, 9, 6, 3, 5 ],
+                            [ 3, 4, 5, 2, 8, 6, 1, 7, 9 ] ]));
 
