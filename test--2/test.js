c1a49a110771617f8feb4916fbf88b5b95562a32
byshlata <byshlata@yandex.ru>
2021-11-21 20:13:29 +0300

    сложение двух чисел представленных строками

--- test--2/test.js
+++ test--2/test.js
@@ -1,23 +1,8 @@
-function twoSum(numbers, target) {
-    let array = numbers;
-    let index = 0;
-    let k = 0;
-    while (k < numbers.length){
-
-        index = numbers[k];
-        numbers[k] = 0;
-    for ( let i = 0; i < numbers.length; i++) {
-        if ( (index + numbers[i]) == target) {
-            return [k, i];
-        }
-        else {numbers[k] = 0;  }
-        
-   }  
-        k++;  
-    }
-   
+function sumStrings(a,b) { 
+  let str = Number (a) + Number (b);
+  
+  return str;
 }
 
-    console.log (twoSum([894, 436, 743, 72, 510, 550, 230, 430, 867, 452], 1610));
+    console.log (sumStrings("123", "456"));
 
-/*     1000037, 1000039 */
